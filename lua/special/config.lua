652e7affcbfcd8e8ad83b2ffb95e8e733675912f
Jonathan L. Komar <Jonathan.Komar@dynatrace.com>
2023-09-26 21:53:46 +0200

    fixup! Fixed

--- lua/special/config.lua
+++ lua/special/config.lua
@@ -4,17 +4,18 @@ _PersistedFiles = _PersistedFiles or {}
 local config = {}
 config.files = _PersistedFiles
 
-local default = {
+local default_files = {
   src = vim.fn.stdpath("data") .. "/special_files",
   cwd_to_path = true,
   effect = function(path)
     vim.cmd("tabedit " .. path)
-  end
+  end,
+  prompt_title = "Special Files"
 }
 
 function config.set_files(files)
   files = vim.F.if_nil(files, {})
-  config.files = vim.tbl_deep_extend("keep", files, default) --keep leftmost
+  config.files = vim.tbl_deep_extend("keep", files, default_files) --keep leftmost
 
   for k, v in pairs(files) do
     print(k)
